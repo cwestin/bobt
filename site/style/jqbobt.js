6acdb1fa60f2ba93044848b6a5e9cd5d79e47e95
Chris Westin <cwestin@yahoo.com>
2011-06-04 15:10:09 -0700

    rewrite to use jQuery

---
+++ site/style/jqbobt.js
@@ -0,0 +1,71 @@
+(function ($) {
+
+    $(document).ready(function () {
+	    var stoc = {
+		init: function() {
+		    /* we need a sequence number for any duplicate headings */
+		    this.headingSeq = 0;
+
+		    /* we detect duplicate headings using this as a hash */
+		    this.headingMap = {};
+		},
+
+		makeHeadingName: function(it) {
+		    var words = it.split(' ');
+		    var name = words.join('');
+
+		    /* if we've used this heading before, make it unique */
+		    if (typeof(this.headingMap[name]) != 'undefined')
+			name = name.concat('-' + this.headingSeq++);
+
+		    /* remember this name is used */
+		    this.headingMap[name] = true;
+		    
+		    return name;
+		},
+
+		doSection: function(toc, section, depth) {
+		    var childLi = $('<li />').appendTo($(toc));
+		    var titleSpan =
+		        $(section).children('span.sectiontoc-title').first();
+		    var titleHtml = $(titleSpan).html();
+		    var titleName = this.makeHeadingName($(titleSpan).text());
+
+		    /* add the link to the toc li */
+		    $('<a />').text(titleHtml).attr( {
+			    'href': '#' + titleName }).
+   		        appendTo(childLi);
+
+		    /* wrap title with the appropriate named anchor heading */
+		    $(titleSpan).wrap('<h' + depth + ' />').
+		        wrap('<a id="' + titleName + '" />');
+
+		    /* recursively do this section's children */
+		    var childToc = $('<ul />');
+		    var childCount = 0;
+		    $(section).children('div.sectiontoc-section').
+		        each(function  (index, section) {
+			    stoc.doSection(childToc, section, depth + 1);
+			    childCount += 1;
+			    return true;
+			});
+
+		    /* if there were children, append them */
+		    if (childCount > 0)
+			$(childToc).appendTo($(childLi));
+		}
+	    };
+
+	    stoc.init();
+
+	    var toc = $('<ul />').appendTo($('div#sectiontoc-toc'));
+	    $('div#sectiontoc-body').children('div.sectiontoc-section').
+		each(function (index, section) {
+		    stoc.doSection(toc, section, 2);
+		    return true;
+		});
+
+	    
+	});
+
+}(jQuery));
--- site/style/sectiontoc/sectiontoc.js
+++ site/style/sectiontoc/sectiontoc.js
@@ -1,111 +1,78 @@
 (function ($) {
 
-    Drupal.behaviors.sectiontocModule = {
+    Drupal.behaviors.sectiontoc = {
 	attach: function (context, settings) {
 
-	    var stoc = {
-		makeHeadingName: function(it) {
-		    var words = it.split(' ');
-		    var name = words.join('');
-
-		    /* if we've used this heading before, make it unique */
-		    if (typeof(this.headingMap[name]) != 'undefined')
-			name = name.concat('-' + this.headingSeq++);
-
-		    /* remember this name is used */
-		    this.headingMap[name] = true;
-
-		    return name;
-		},
-
-		doSection: function(tocParent, parentSection, depth) {
-		    var allSections =
-		        parentSection.getElementsByClassName(
-							 'sectiontoc-section');
-		    for(var sectionIndex in allSections) {
-			var section = allSections[sectionIndex];
-
-			/*
-			  getElementsByClassName() returns all descendants; we
-			  only want the direct descendants, because we will do
-			  this recursively for each one to maintain the toc
-			  structure in parallel.
-			*/
-			if (section.parentNode != parentSection)
-			    continue;
-
-			/* find the span that has the heading */
-			var allTitles =
-			    section.getElementsByClassName('sectiontoc-title');
-			if (typeof(allTitles[0]) != 'undefined') {
-			    var title = allTitles[0];
-
-			    /* we only want to see the immediate descendants */
-			    if (title.parentNode != section)
-				continue;
-
-			    /* create the heading that will replace the span */
-			    var headingA = document.createElement('a');
-			    var headingName =
-				this.makeHeadingName(title.innerText);
-			    headingA.setAttribute('id', headingName);
-			    headingA.innerHTML = title.innerHTML;
-			    var sectionHeading =
-				document.createElement('h' + depth);
-			    sectionHeading.appendChild(headingA);
-
-			    /* replace the span with the named heading */
-			    section.insertBefore(sectionHeading, title);
-			    section.removeChild(title);
-
-			    /* create the table of contents link */
-			    var tocLiA = document.createElement('a');
-			    tocLiA.setAttribute('href', '#' + headingName);
-			    tocLiA.innerHTML = headingA.innerHTML;
-
-			    /* add reference to the toc */
-			    var tocLi = document.createElement('li');
-			    tocLi.appendChild(tocLiA);
-			    tocParent.appendChild(tocLi);
-
-			    /* do the child sections below this */
-			    var childUl = document.createElement('ul');
-			    this.doSection(childUl, section, depth + 1);
-			    if (childUl.childElementCount > 0)
-				tocLi.appendChild(childUl);
-			}
-		    }
-		},
-
-		doHeaders: function() {
-		    /* we need a sequence number for any duplicate headings */
-		    this.headingSeq = 0;
-
-		    /* we detect duplicate headings using this as a hash */
-		    this.headingMap = {};
-
-		    /* find the toc, if there is one */
-		    var toc = document.getElementById('sectiontoc-toc');
-		    var tocUl = document.createElement('ul');
-		    if ((typeof(toc) != 'undefined') &&
-			(toc.nodeName == 'DIV')) {
-			toc.appendChild(tocUl);
-			toc.appendChild(document.createElement('hr'));
-		    }
-
-		    /* find and set all the section titles */
-		    this.doSection(tocUl, document.body, 2);
-		},
-
-		doReady: function() {
-		    this.doHeaders();
-		}
-	    }
-
 	    $(document).ready(function () {
-		    stoc.doReady();
-		});
-	}
-    };
+		    var stoc = {
+			init: function() {
+			    /* a sequence number for any duplicate headings */
+			    this.headingSeq = 0;
+
+			    /* detect duplicate headings using this as a hash */
+			    this.headingMap = {};
+			},
+
+			makeHeadingName: function(it) {
+			    var words = it.split(' ');
+			    var name = words.join('');
+
+			    /* if we've used this before, make it unique */
+			    if (typeof(this.headingMap[name]) != 'undefined')
+				name = name.concat('-' + this.headingSeq++);
+
+			    /* remember this name is used */
+			    this.headingMap[name] = true;
+		    
+			    return name;
+			},
+
+			doSection: function(toc, section, depth) {
+			    var childLi = $('<li />').appendTo($(toc));
+			    var titleSpan =
+			    $(section).children('span.sectiontoc-title').
+			        first();
+			    var titleHtml = $(titleSpan).html();
+			    var titleName = this.makeHeadingName(
+			        $(titleSpan).text());
+
+			    /* add the link to the toc li */
+			    $('<a />').text(titleHtml).attr( {
+				    'href': '#' + titleName }).
+			        appendTo(childLi);
+
+			    /* wrap title with named anchor heading */
+			    $(titleSpan).wrap('<h' + depth + ' />').
+			        wrap('<a id="' + titleName + '" />');
+
+			    /* recursively do this section's children */
+			    var childToc = $('<ul />');
+			    var childCount = 0;
+			    $(section).children('div.sectiontoc-section').
+			        each(function  (index, section) {
+				    stoc.doSection(
+				        childToc, section, depth + 1);
+				    childCount += 1;
+				    return true;
+				    });
+
+			    /* if there were children, append them */
+			    if (childCount > 0)
+				$(childToc).appendTo($(childLi));
+			}
+		    };
+
+		    stoc.init();
+
+		    var toc = $('<ul />').appendTo($('div#sectiontoc-toc'));
+		    $('div#sectiontoc-body').children('div.sectiontoc-section').
+		        each(function (index, section) {
+				stoc.doSection(toc, section, 2);
+				return true;
+			    });
+		}); /* $(document).ready(...) */
+
+	} /* attach */
+    }; /* Drupal.behaviors.sectiontoc */
 
 }(jQuery));
