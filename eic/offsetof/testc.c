27298814064f3152f056ac00f8b261362adf35e7
Chris Westin <cwestin@yahoo.com>
2011-06-12 12:27:44 -0700

    fix makefile and annotate examples

--- eic/offsetof/testc.c
+++ eic/offsetof/testc.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stddef.h>
 
 struct foo
--- eic/offsetof/testcpp.cpp
+++ eic/offsetof/testcpp.cpp
@@ -38,11 +38,28 @@ Bar::Bar(int &_z):
 
 void Bar::printOffsets() const
 {
-  //cout << "offsetof(Bar, x) == " << offsetof(Bar, x) << endl;
-  //cout << "offsetof(Bar, x) == " << cppOffsetOf(Bar, x) << endl;
+  // cout << "offsetof(Bar, x) == " << offsetof(Bar, x) << endl;
+  /*
+    g++ 3.4.4 says:
+    testcpp.cpp:41: warning: invalid access to non-static data member `Bar::x' of NULL object
+    testcpp.cpp:41: warning: (perhaps the `offsetof' macro was used incorrectly)
+  */
+
+  // cout << "offsetof(Bar, x) == " << cppOffsetOf(Bar, x) << endl;
+  /*
+    g++ 3.4.4 says:
+    testcpp.cpp:48: warning: invalid access to non-static data member `Bar::x' of NULL object
+    testcpp.cpp:48: warning: (perhaps the `offsetof' macro was used incorrectly)
+  */
+
   cout << "offsetof(Bar, x) == " << myOffsetOf(Bar, x) << endl;
+
   cout << "offsetof(Bar, y) == " << myOffsetOf(Bar, y) << endl;
-  //cout << "offsetof(Bar, z) == " << myOffsetOf(Bar, z) << endl;
+
+  // cout << "offsetof(Bar, z) == " << myOffsetOf(Bar, z) << endl;
+  /*
+    This would crash at run-time.
+  */
 }
 
 void Bar::staticPrintOffsets()
@@ -59,6 +76,10 @@ const int Bar::offsets[] =
 
 int main()
 {
+  /*
+    The classical definition works fine for PODs:  "Plain Old Data
+    structures"
+  */
   cout << "offsetof(Foo, a) == " << offsetof(Foo, a) << endl;
   cout << "offsetof(Foo, b) == " << offsetof(Foo, b) << endl;
   cout << "offsetof(Foo, c) == " << offsetof(Foo, c) << endl;
@@ -70,6 +91,11 @@ int main()
   Bar::staticPrintOffsets();
 
   // cout << "non-member offsetof(Bar, y) == " << myOffsetOf(Bar, y) << endl;
+  /*
+    g++ 3.4.4 says:
+    testcpp.cpp:25: error: `int Bar::y' is private
+    testcpp.cpp:93: error: within this context
+  */
 
   return 0;
 }
