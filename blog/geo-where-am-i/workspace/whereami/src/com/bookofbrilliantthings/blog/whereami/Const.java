01b1c31cad9aff0768c9f3cb092612b1c9c9a8fd
U-tellus\cwestin <cwestin@10gen.com>
2012-05-26 21:52:14 -0400

    fixed cygwin protection bits

--- blog/geo-where-am-i/workspace/whereami/src/com/bookofbrilliantthings/blog/whereami/Const.java
+++ blog/geo-where-am-i/workspace/whereami/src/com/bookofbrilliantthings/blog/whereami/Const.java
@@ -2,5 +2,13 @@ package com.bookofbrilliantthings.blog.whereami;
 
 public class Const
 {
-	final static String SHAPEFILE_NAME = "C:/extracted/state_bounds/state_bounds.shp";
+	// file downloaded from USGS - doesn't include boundaries for coastal states
+	//final static String SHAPEFILE_NAME = "C:/extracted/state_bounds/state_bounds.shp";
+
+	final static String SHAPEFILE_NAME = "C:/extracted/geotools-project/geotools-2.7.4/" +
+		"modules/library/sample-data/src/main/resources/org/geotools/test-data/shapes/statepop.shp";
+
+	// for connecting to MongoDB
+	final static String MONGODB_HOST = "localhost";
+	final static int MONGODB_PORT = 27017;
 }
--- blog/geo-where-am-i/workspace/whereami/src/com/bookofbrilliantthings/blog/whereami/DataLoader.java
+++ blog/geo-where-am-i/workspace/whereami/src/com/bookofbrilliantthings/blog/whereami/DataLoader.java
@@ -1,26 +1,44 @@
 package com.bookofbrilliantthings.blog.whereami;
 
 import java.io.File;
+import java.io.PrintStream;
 import java.io.Serializable;
 import java.util.HashMap;
 
 import org.geotools.data.DataStore;
 import org.geotools.data.DataStoreFinder;
-import org.geotools.data.FeatureSource;
-import org.geotools.data.FileDataStore;
-import org.geotools.data.FileDataStoreFinder;
 import org.geotools.data.simple.SimpleFeatureCollection;
 import org.geotools.data.simple.SimpleFeatureIterator;
 import org.geotools.data.simple.SimpleFeatureSource;
-import org.geotools.feature.FeatureCollection;
-import org.geotools.feature.FeatureIterator;
-import org.opengis.feature.Feature;
-import org.opengis.feature.GeometryAttribute;
 import org.opengis.feature.simple.SimpleFeature;
-import org.opengis.geometry.Geometry;
+import org.opengis.feature.simple.SimpleFeatureType;
+import org.opengis.feature.type.AttributeDescriptor;
+import org.opengis.feature.type.AttributeType;
+
+import com.mongodb.BasicDBList;
+import com.mongodb.BasicDBObject;
+
+import com.vividsolutions.jts.geom.Coordinate;
+import com.vividsolutions.jts.geom.MultiPolygon;
+import com.vividsolutions.jts.geom.Point;
 
 public class DataLoader
 {
+	static void printSchema(PrintStream printStream, SimpleFeatureSource featureSource)
+	{
+		final SimpleFeatureType featureType = featureSource.getSchema();
+		printStream.println("FID\t");
+		for (int i = 0; i < featureType.getAttributeCount(); i++)
+		{
+			final AttributeDescriptor attrDesc = featureType.getDescriptor(i);
+			final String attrName = attrDesc.getLocalName();
+			final AttributeType attrType = attrDesc.getType();
+			printStream.println("attrName == " + attrName);
+			printStream.println("attrType name " + attrType.getName());
+		}
+		printStream.println();
+	}
+	
 	public static void main(String argv[])
 	{
 		/*
@@ -41,28 +59,83 @@ public class DataLoader
 			connectParameters.put("url", file.toURI().toURL());
 			DataStore dataStore = DataStoreFinder.getDataStore(connectParameters);	
 			assert dataStore != null;
-			String[] typeNames = dataStore.getTypeNames();
-			String typeName = typeNames[0];
+			final String[] typeNames = dataStore.getTypeNames();
+
+			for(String typeName : typeNames)
+				System.out.println("typeName " + typeName);
+			System.out.println();
+
+			final String typeName0 = typeNames[0];
+			System.out.println("Reading content " + typeName0);
 		
-			System.out.println("Reading content " + typeName);
-		
-			SimpleFeatureSource featureSource = dataStore.getFeatureSource(typeName);
-			SimpleFeatureCollection collection = featureSource.getFeatures();
-			SimpleFeatureIterator iterator = collection.features();
-		
-		
+			final SimpleFeatureSource featureSource = dataStore.getFeatureSource(typeName0);
+
+			// print out the list of features so we can see what's there
+			printSchema(System.out, featureSource);
+
+			final SimpleFeatureCollection collection = featureSource.getFeatures();
+			final SimpleFeatureIterator iterator = collection.features();
+
+			// set up our connection to MongoDB
+			MongoGeo mongoGeo = new MongoGeo(Const.MONGODB_HOST, Const.MONGODB_PORT);
+			
+			// create the geospatial index
+			mongoGeo.createGeoIndex();
+
 			try
 			{
 				while(iterator.hasNext())
 				{
 					SimpleFeature feature = iterator.next();
-					//Geometry sourceGeometry = (Geometry)feature.getDefaultGeometry();
-					System.out.println(feature.getID());
+					
+					/*
+					 * From previously printing out the schema, I've seen what look like the
+					 * interesting attributes for each feature, and am going to ask for those.
+					 */
+					final String stateName = (String)feature.getAttribute("STATE_NAME");
+					final String stateAbbr = (String)feature.getAttribute("STATE_ABBR");
+					final MultiPolygon multiPolygon = (MultiPolygon)feature.getAttribute("the_geom");
+					final int nPoints = multiPolygon.getNumPoints();
+					final Point centroid = multiPolygon.getCentroid();
+					System.out.println("STATE_NAME " + stateName + ", STATE_ABBR == " + stateAbbr +
+							", nPoints == " + nPoints +
+							", centroid == (" + centroid.getX() + ", " + centroid.getY() + ")");
+					
+					final Coordinate coordinates[] = multiPolygon.getCoordinates();
+					assert coordinates.length == nPoints;
+					
+					// prepare a database object for insertion into MongoDB
+					final BasicDBObject boundObj = new BasicDBObject();
+					boundObj.put("stateAbbr", stateAbbr);
+					boundObj.put("stateName", stateName);
+					boundObj.put("country", "USA");
+
+					final BasicDBList dbCentroid = new BasicDBList();
+					dbCentroid.add(centroid.getX());
+					dbCentroid.add(centroid.getY());
+
+					boundObj.put("centroid", dbCentroid);
+					
+					final BasicDBList dbPoly = new BasicDBList();
+					for(Coordinate coordinate : coordinates)
+					{
+						final BasicDBList dbPoint = new BasicDBList();
+						dbPoint.add(coordinate.x);
+						dbPoint.add(coordinate.y);
+
+						dbPoly.add(dbPoint);
+					}
+					
+					boundObj.put("poly", dbPoly);
+					
+					// insert the object
+					mongoGeo.insertRegion(boundObj);
 				}
 			}
 			finally
 			{
 				iterator.close();
+				mongoGeo.close();
 			}
 		}
 		catch(Throwable e)
